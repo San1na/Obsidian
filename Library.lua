b34bb1adaefde704fb487a600660556fbdc35d1c
titela2021 <gujejianisandro@gmail.com>
2026-05-11 18:29:43 +0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,4 +1,3 @@
--- 6
 local cloneref = (cloneref or clonereference or function(instance: any)
     return instance
 end)
@@ -6445,7 +6444,7 @@ do
         local PreviewCam = Instance.new("Camera")
         PreviewCam.CameraType = Enum.CameraType.Scriptable
         PreviewCam.FieldOfView = 50
-        PreviewCam.CFrame = CFrame.new(0.2, -0.35, -11) * CFrame.Angles(0, -math.pi, 0)
+        PreviewCam.CFrame = CFrame.new(0.2, -0.35, -12) * CFrame.Angles(0, -math.pi, 0)
         PreviewCam.Parent = CharViewport
         CharViewport.CurrentCamera = PreviewCam
 
